23437cc775a62a17e392e4bbd8400bc55d00baef
Tahir <tahircreativedev577@gmail.com>
2023-04-09 16:57:05 +0500

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -81,9 +81,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "adad92358b3b47ae891bade9ceb1ef16",
-"/": "adad92358b3b47ae891bade9ceb1ef16",
-"main.dart.js": "b912807dc80ffbcb2daad1a61e2988ca",
+"index.html": "8839eba58a0594b74372663ab9c14f6f",
+"/": "8839eba58a0594b74372663ab9c14f6f",
+"main.dart.js": "23576b3ccb30e0436b82ee6e517bf8ba",
 "manifest.json": "e26eb3120bd0f9028be66d1299822577",
 "version.json": "44f1005a238451c293680ad111867fd2"
 };
